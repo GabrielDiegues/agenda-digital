2c40107eb39c02dd5cdbce8d6f58071554f56363
Gabriel <gabrieldiegues12@gmail.com>
2025-08-14 21:09:21 -0300

    Programa Agenda

---
+++ ConversorFusoHorario/Agenda.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorFusoHorario
+{
+    public class Agenda
+    {
+        List<IAgendaEntrada> agenda;
+
+        Dictionary<int, string> timezones;
+
+        // Funções
+        public int criarMenu(string opcoes, int totalOpcoes)
+        {
+            int inputUsuario = 0;
+            do
+            {
+                Console.WriteLine(opcoes);
+                try
+                {
+                    inputUsuario = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Por favor, insira um número válido");
+                }
+            }
+            while (!(inputUsuario >= 1 && inputUsuario <= totalOpcoes));
+            return inputUsuario;
+        }
+
+
+        public void exibirCompromissosHoje(int timeZone)
+        {
+            string? fuso = timezones.ContainsKey(timeZone) ? timezones[timeZone] : null;
+            foreach (IAgendaEntrada compromisso in agenda)
+            {
+                compromisso.Imprimir(fuso);
+            }
+        }
+
+
+        public void exibirCompromissosEspecifico(DateTime data, int timeZone)
+        {
+            string? fuso = timezones.ContainsKey(timeZone) ? timezones[timeZone] : null;
+            foreach (IAgendaEntrada compromisso in agenda)
+            {
+
+                if (compromisso.DataHora.Date == data.Date)
+                {
+                    Console.WriteLine(compromisso);
+                }
+            }
+        }
+
+
+        public int menuTimezone()
+        {
+            int inputUsuario = criarMenu("Escolha um timezone:\n1- Brasil\n2- Canadá", 2);
+            return inputUsuario;
+        }
+
+
+        public DateTime dataUsuario()
+        {
+            DateTime dataConvertida;
+            string dataUsuario = "";
+            do
+            {
+                Console.WriteLine("Digite uma data no formato: dd/mm/yyyy");
+                dataUsuario = Console.ReadLine();
+            } while (DateTime.TryParseExact(dataUsuario, "dd/mm/yyyy",
+            System.Globalization.CultureInfo.InvariantCulture,
+            System.Globalization.DateTimeStyles.None, out dataConvertida));
+
+            return dataConvertida;
+        }
+
+
+        // Construtor
+        public Agenda()
+        {
+            this.agenda = new List<IAgendaEntrada>();
+            this.timezones = new Dictionary<int, string>{
+                { 1, "BR" },
+                {2, "CA" }
+            };
+        }
+    }
+}
--- ConversorFusoHorario/Program.cs
+++ ConversorFusoHorario/Program.cs
@@ -1,5 +1,45 @@
-﻿using System;
+﻿// Fazer a opção de criar compromisso
+
+using System;
 using ConversorFusoHorario;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// Main program
+class Program
+{
+    static void Main()
+    {
+        Agenda agenda = new Agenda();
+        while (true)
+        {
+            Console.WriteLine("Bem-vindo a agenda");
+            int opcaoUsuario = agenda.criarMenu("1- Exibir compromissos\n2- Criar um compromisso", 2);
+
+            switch (opcaoUsuario)
+            {
+                case 1:
+                    opcaoUsuario = agenda.criarMenu("1- Exibir compromissos do dia de hoje\n2- Exibir compromissos de um dia em específico", 2);
+                    int opcaoTimezone = agenda. menuTimezone();
+                    if (opcaoUsuario == 1)
+                    {
+                        agenda.exibirCompromissosHoje(opcaoTimezone);
+                    }
+
+                    else
+                    {
+                        DateTime dataConvertida = agenda.dataUsuario();
+                        agenda.exibirCompromissosEspecifico(dataConvertida, opcaoTimezone);
+                    }
+                    break;
+
+                case 2:
+                    DateTime data = agenda.dataUsuario();
+
+
+                    break;
+            }
+        }
+    }
+}
